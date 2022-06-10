dfba4c549b9052f506fd8eed12540487293fe9b5
Kropyls <Kropyls@gmail.com>
2022-06-09 20:39:31 -0700

    got in-line checks for winner out of the way. Now just need to do diagonals.

--- game.rb
+++ game.rb
@@ -4,6 +4,7 @@
 class Board
   def initialize
     @board_as_array = Array.new(3) { Array.new(3, '+') }
+    @player_wins = 0
   end
 
   def print_line
@@ -27,9 +28,22 @@ class Board
   def print_row(pos)
     @board_as_array[pos]
   end
+
+  def check_for_win_line(player_letter)
+    @board_as_array.reduce(Array.new(3, player_letter)) do |acc, row|
+      return true if row.all? { |letter| letter == player_letter }
+
+      row.each_with_index.map { |letter, i| letter == acc[i] ? letter : false }
+    end.any?(player_letter)
+  end
 end
 
 board = Board.new
 board.print_board
 board.write_board('A', 1, 1)
 board.print_board
+board.write_board('A', 1, 2)
+board.print_board
+board.write_board('A', 1, 0)
+board.print_board
+puts board.check_for_win_line('A')
