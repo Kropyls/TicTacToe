bbde02b042add19def5cf15b6c0df312414c0c4a
Kropyls <Kropyls@gmail.com>
2022-06-16 20:23:43 -0700

    Got it working, but need to improve time-complexity of check_win method.

--- game.rb
+++ game.rb
@@ -45,15 +45,15 @@ puts 'Note the positions above. The numbers are how you choose your positions'
 until game_over == true
   board.write_board(player1, get_selection('Player 1', board.board_as_array, CHOICES))
   board.print_board
+  if board.check_for_win(player1)
+    puts 'Player 1 wins!'
+    game_over = true
+    break
+  end
   board.write_board(player2, get_selection('Player 2', board.board_as_array, CHOICES))
   board.print_board
+  if board.check_for_win(player2)
+    puts 'Player 2 wins!'
+    game_over = true
+  end
 end
-
-board.print_board
-board.write_board('A', [0, 2])
-board.print_board
-board.write_board('A', [1, 1])
-board.print_board
-board.write_board('A', [2, 0])
-board.print_board
-puts board.check_for_win('A')
